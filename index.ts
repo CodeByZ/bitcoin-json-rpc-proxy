811335c7814bcc044615f5420ff5a35f4a4629ed
Pickholz <ZPickholz@Verint.com>
2017-12-19 21:01:22 +0200

    some updates

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const Client = require('bitcoin-core');
 
-const port = process.env.PORT || 1972;        // set our port
+const app = express();
+
+const port = process.env.PORT || 8080;
 const BASE_HREF = '/api';
 
 let isConnected: boolean = false;
@@ -54,6 +55,10 @@ app.use(function (err, req, res, next) {
     res.status(500).send('Error: ' + err.message);
 });
 
+app.use('/health', (req, res) => {
+    res.json({alive: true});
+});
+
 // check if we're connected (middleware)
 app.use((req, res, next) => {
     if (isConnected || req.path.toLowerCase() === BASE_HREF + '/connect') {
@@ -87,6 +92,27 @@ router.get('/get-blockchain-info', (req, res) => {
 
 router.get('/get-block/:blockhash', (req, res) => {
     client.getBlock(req.params.blockhash, true)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(error => {
+            res.status(400).json({error});
+        })
+});
+
+router.get('/get-received-by-address/:address', (req, res) => {
+    client.getReceivedByAddress(req.params.address, true)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(error => {
+            res.status(400).json({error});
+        })
+});
+
+
+router.get('/get-block-count', (req, res) => {
+    client.getBlockCount()
         .then(data => {
             res.json(data);
         })
